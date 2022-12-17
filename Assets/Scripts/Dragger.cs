6b0d85367389598bd72cc5731806af792bc5d6a1
Adam <loginadas@gmail.com>
2022-12-17 23:58:24 +0100

    chooseInventorySystem

--- Assets/Scripts/Dragger.cs
+++ Assets/Scripts/Dragger.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
+using ReadOnlyAttribute = Unity.Collections.ReadOnlyAttribute;
 
 public class Dragger : MonoBehaviour
 {
-    bool aboveBackpack, onTable, isDynamic;
+    [ReadOnly] [SerializeField] bool aboveBackpack, onTable, isDynamic;
     Vector3 startingPos;
 
     private void Start()
@@ -14,50 +17,76 @@ public class Dragger : MonoBehaviour
         onTable = true;
     }
 
+    private void OnMouseDown()
+    {
+        gameObject.GetComponent<Collider2D>().isTrigger = true;
+    }
+
     private void OnMouseDrag()
     {
         transform.position = GetMousePos();
         if (isDynamic)
         {
-            isDynamic= false;
-            gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+            ChangeToKinematic();
         }
     }
 
     private void OnMouseUp()
     {
+        gameObject.GetComponent<Collider2D>().isTrigger = false;
         if (aboveBackpack)
         {
-            isDynamic = true;
-            gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
-            gameObject.GetComponent<Rigidbody2D>().collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+            ChangeToDynamic();
         }
-        else if (!onTable)
-            transform.position= startingPos;
+        else ResetPos();
+            
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log(collision.gameObject.layer);
         if (collision.gameObject.layer == 7) 
             aboveBackpack = true;
         else if (collision.gameObject.layer == 8) 
             onTable = true;
+        else if (collision.gameObject.CompareTag("OutOfBounds")) ResetPos();
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        Debug.Log(collision.gameObject.layer);
         if (collision.gameObject.layer == 7)
             aboveBackpack = false;
         else if (collision.gameObject.layer == 8)
             onTable = false;
+        else if (collision.gameObject.CompareTag("OutOfBounds")) ResetPos();
     }
 
-    Vector3 GetMousePos()
+    private Vector3 GetMousePos()
     {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePos.z = 0;
         return mousePos;
     }
+
+    private void ResetPos()
+    {
+        ChangeToKinematic();
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0,0,0); 
+        transform.position = startingPos;
+        transform.rotation = Quaternion.identity;
+    }
+
+    private void ChangeToKinematic()
+    {
+        isDynamic = false;
+        gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+    }
+    
+    private void ChangeToDynamic()
+    {
+        isDynamic = true;
+        gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+        gameObject.GetComponent<Rigidbody2D>().collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+    }
+
+    
 }
